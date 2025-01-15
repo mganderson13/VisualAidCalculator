8bdbdb352988b5279f3ebbbb4189720e179459d6
Marianne Anderson <mganderson13@gmail.com>
2025-01-15 13:50:51 -0600

    script file with calculator functions

---
+++ script.js
@@ -0,0 +1,137 @@
+
+let numA = "";
+let onNumA = true;
+let numB = "";
+let operator = "";
+let currentDisplay = document.getElementById("display");
+let outerVisual = document.getElementById("outerVisual");
+let equationVisual = document.getElementById("equationVisual");
+let pictureVisual = document.getElementById("pictureVisual");
+let dec = document.getElementById("dec");
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    if(b == 0) {
+        return "uh, no";
+    } else {
+        return a / b;
+    };
+};
+
+function doOperation(firstNum, secondNum, operation) {
+    if(operation === "+") {
+        return add(firstNum, secondNum);
+    } else if(operation === "-") {
+        return subtract(firstNum, secondNum);
+    } else if(operation === "*") {
+        return multiply(firstNum, secondNum);
+    } else if(operation === "/") {
+        return divide(firstNum, secondNum);
+    } else {
+        return "huh?";
+    }
+}
+
+function readNum(sentNum) {
+    if(onNumA === true) {
+        numA += sentNum;
+        currentDisplay.innerHTML = numA;
+        equationVisual.innerHTML = numA;
+    } else {
+        numB += sentNum;
+        currentDisplay.innerHTML = numA + operator + numB;
+        equationVisual.innerHTML = numA + " " + operator + " " + numB;
+    } 
+}
+
+function readOper(sentOper) {
+    if(operator) {
+        currentDisplay.innerHTML = "ERROR";
+        equationVisual.innerHTML = "ERROR";
+    } else {
+        operator = sentOper;
+        onNumA = false;
+        currentDisplay.innerHTML = numA + operator;
+        equationVisual.innerHTML = numA + " " + operator;
+        //create circles/dots/numberline
+        if(operator === "*") {
+        createMultCircles();
+        }
+        if(operator === "+" && numA < 1000) {
+            createAddLine();
+        }
+        if(operator === "-" && numA < 1000) {
+            createSubtractionLine();
+        }
+    }
+}
+
+function readEnter() {
+    let answer = doOperation(parseFloat(numA), parseFloat(numB), operator);
+    if(typeof(answer) === "number") {
+        if(operator === "*"){
+            createMultDots(numB);
+        currentDisplay.innerHTML = Number(Math.round(answer + 'e' + 7) + "e-" + 7);
+        equationVisual.innerHTML = numA + " " + operator + " " + numB + " " + "=" + " " + Number(Math.round(answer + 'e' + 7) + "e-" + 7);
+        }
+        if (operator === "+") {
+            if (numA < 1000 && numB < 1000) {
+            fillInNumberLine(numB);
+            }
+            currentDisplay.innerHTML = Number(Math.round(answer + 'e' + 7) + "e-" + 7);
+            equationVisual.innerHTML = numA + " " + operator + " " + numB + " " + "=" + " " + Number(Math.round(answer + 'e' + 7) + "e-" + 7);
+        }
+        if (operator === "-") {
+            if (numA < 1000 && numB < 1000) {
+            fillInSubtractionNumberLine(numB);
+            }
+            currentDisplay.innerHTML = Number(Math.round(answer + 'e' + 7) + "e-" + 7);
+            equationVisual.innerHTML = numA + " " + operator + " " + numB + " " + "=" + " " + Number(Math.round(answer + 'e' + 7) + "e-" + 7);
+        }
+    } else {
+        currentDisplay.innerHTML = "ERROR";
+        equationVisual.innerHTML = "ERROR";
+        numA = "";
+    }  
+    //reset values in function and run with setTimeout so dont mess up display
+    function resetValues() {
+        numA= "";
+        onNumA = true;
+        numB = "";
+        operator = "";
+    }
+    resetValues();
+}
+
+function readClear() {
+    numA = "";
+    numB = "";
+    operator = "";
+    currentDisplay.innerHTML = "";
+    equationVisual.innerHTML = "";
+    pictureVisual.innerHTML = "";
+    onNumA = true;
+}
+
+function readBackspace() {
+    if(onNumA === true) {
+        numA = numA.slice(0, -1);
+        currentDisplay.innerHTML = numA;
+        equationVisual.innerHTML = numA;
+    } else {
+        numB = numB.slice(0, -1);
+        currentDisplay.innerHTML = numA + operator + numB;
+        equationVisual.innerHTML = numA + " " + operator + " " + numB;
+    }
+}
